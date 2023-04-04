665abe698bcc5a8e34220f8b5200eb6e38242b8d
eolubi <emmanuelolubi@yahoo.com>
2023-04-04 08:37:11 +0100

    Completed the script for functionality, formatted with prettier

--- script.js
+++ script.js
@@ -1,6 +1,126 @@
-const reset = document.getElementById("#reset");
-console.log(reset);
-const price = document.getElementsByClassName("price");
-reset.addEventListener("click",function(){
-    price.value="";
-})
+//Referencing HTML elements
+const billInput = document.getElementById('bill-input');
+const button = document.querySelectorAll('.btn');
+const people = document.getElementById('people-input');
+const customTip = document.getElementById('tip-input');
+const peopleError = document.getElementById('people-error');
+const totalValue = document.querySelector('.total-value');
+const tipValue = document.querySelector('.tip-value');
+const resetBtn = document.querySelector('.reset-btn')
+
+//Set initial values for bill, people, and tip
+let billVal=0;
+let peopleVal= 1 ;
+let tipVal=0.15;
+
+//Input events listener on bill input field
+billInput.addEventListener('input', validateBill);
+
+//Function for validating and update bill value
+function validateBill() {
+    if (billInput.value.includes(',')){
+        billInput.value = billInput.value.replace(',',',');
+    }
+    billVal = parseFloat(billInput.value);
+    calculate();
+}
+
+//Input events listener on custom tip input field
+customTip.addEventListener('input', tipCustomVal);
+
+//Function for updating tip value when custom tip is selected
+function tipCustomVal() {
+    tipVal = parseFloat(customTip.value) / 100;
+    button.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+    if (customTip.value !== '') {
+      calculate();
+    }
+  }
+
+// Listen for input events on number of people input field
+people.addEventListener('input', setPeopleVal);
+
+// Function to update people value
+function setPeopleVal() {
+  peopleVal = parseFloat(people.value);
+  if (peopleVal <= 0) {
+    peopleError.innerHTML = 'number must be greater than zero';
+    setTimeout(function () {
+      peopleError.innerHTML = '';
+    }, 2000);
+  }
+  calculate();
+}
+
+// Listen for click events on tip percentage buttons
+button.forEach((btn) => {
+    btn.addEventListener('click', handleClick);
+  });
+  
+  // Function to handle click events on tip percentage buttons
+  function handleClick(event) {
+    button.forEach((btn) => {
+      btn.classList.remove('active');
+      if (event.target.innerHTML === btn.innerHTML) {
+        btn.classList.add('active');
+        tipVal = parseFloat(btn.innerHTML) / 100;
+      }
+    });
+    customTip.value = '';
+    calculate();
+  }
+
+// Calculate tip and total amounts based on bill, tip, and number of people
+function calculate() {
+    if (billVal && peopleVal >= 1) {
+      let tip = billVal * tipVal / peopleVal;
+      let totalAmount = billVal * (tipVal + 1) / peopleVal;
+  
+      tipValue.innerHTML = tip.toFixed(2);
+      totalValue.innerHTML = totalAmount.toFixed(2);
+    } else {
+      tipValue.innerHTML = '0.00';
+      totalValue.innerHTML = '0.00';
+    }
+  }
+
+// Reset all input values to default and disable the reset button
+function handleReset() {
+    resetBtn.disabled = true;
+    resetBtn.style.backgroundColor = 'darkcyan';
+  
+    billInput.value = '';
+    validateBill();
+  
+    button.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+    customTip.value = '';
+  
+    people.value = '1';
+    setPeopleVal();
+  
+    calculate();
+  }
+  
+  // Listen for input events on bill, customTip, and people input fields
+  billInput.addEventListener('input', enableReset);
+  customTip.addEventListener('input', enableReset);
+  people.addEventListener('input', enableReset);
+  
+  // Function to enable reset button and set the background color to its original state
+  function enableReset() {
+    if (resetBtn.disabled) {
+      resetBtn.disabled = false;
+      resetBtn.style.backgroundColor = ''; //set this to the original background color
+    }
+  }
+  
+  // Initialize the reset button as disabled and dark cyan
+  resetBtn.disabled = true;
+  resetBtn.style.backgroundColor = 'darkcyan';
+  
+  // Listen for click events on reset button
+  resetBtn.addEventListener('click', handleReset);  
